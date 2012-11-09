a76c59d9062bf1d4a88c53d1fabf5a911ac21668
David CK Chan <dchan@staging-resources.webadmin.vno.com>
2012-11-09 17:21:33 -0500

    Now handles history.popstate (back/forward browser buttons), removed callback support, adds onReplace event for customized callback, unsupported browser anchors would simply use default behavior

--- Source/PiStory.js
+++ Source/PiStory.js
@@ -32,39 +32,81 @@ var PiStory = new Class
 
 // Static vars and functions
 PiStory.extend({
+	_elements: [],
+	findElementByKey: function(key) {
+		var _found = null;
+		PiStory._elements.some(function(_element) {
+			if (_element.retrieve('PiStoryKey') == key) {
+				_found = _element;
+				return true;
+			}
+			return false;
+		});
+		return _found;
+	}
 });
 
 PiStory.implement({
   initialize: function(elements, options)
   {
-    this.setOptions(options);
-    var opts = this.options;
+	if (window.history && window.history.pushState) {
+	    this.setOptions(options);
+	    var opts = this.options;
 
-    this._elements = $$(elements);
-    if (this._elements && this._elements.length) {
-      this.build();
-    }
+	    PiStory._elements = $$(elements);
+	    if (PiStory._elements && PiStory._elements.length) {
+	      this.build();
+	    }	
+	}
+	else {
+		return;
+	}
   },
   build: function() {
-    if (window.history && window.history.pushState) {
+
+	  var dataOpts = this.options.dataAttributes;
+	  history.pushState({
+		url: location.href,
+	    source: document.body.getAttribute(dataOpts.source),
+	    destination: document.body.getAttribute(dataOpts.destination)
+	  }, "", location.href);
+	
       this.buildPushState();
+	  this.buildPopState();
       return;
-    }
-    
+
   },
   buildPushState: function() {
-    this._elements.each(function(_anchor) {
+    PiStory._elements.each(function(_anchor, i) {
+	  _anchor.store('PiStoryKey', i);
       _anchor.addEvent('click', this.handlePushState.bind(this, _anchor));
     }.bind(this));
+  },
+  buildPopState: function() {
+	Element.NativeEvents.popstate = 2;
+    window.addEvent('popstate', this.handlePopState.bind(this));
+  },
+  handlePopState: function(obj) {
+	if (obj.event.state) {
+		var _element = PiStory.findElementByKey(obj.event.state.key) || document.body;
+
+	    new Request.HTML({
+	      url: obj.event.state.url,
+	      evalScripts: false,
+	      onSuccess: this.replaceContent.bind(this, _element)
+	    }).get();	
+	}
   },
   handlePushState: function(_anchor, e) {
     e.stop();
     var dataOpts = this.options.dataAttributes;
     var stateObj = {
+      key: _anchor.retrieve('PiStoryKey'),
+      url: _anchor.get('href'),
       source: _anchor.getAttribute(dataOpts.source),
       destination: _anchor.getAttribute(dataOpts.destination)
     };
-    history.pushState(stateObj, "", _anchor.get('href'));
+	history.pushState(stateObj, "", _anchor.get('href'));
     
     new Request.HTML({
       url: _anchor.get('href'),
@@ -72,13 +114,13 @@ PiStory.implement({
       onSuccess: this.replaceContent.bind(this, _anchor)
     }).get();
   },
-  replaceContent: function(_anchor, responseTree, responseElements, responseHTML, responseJavaScript) {
+  replaceContent: function(_element, responseTree, responseElements, responseHTML, responseJavaScript) {
     var dataOpts = this.options.dataAttributes;
-    var sourceSelector = _anchor.getAttribute(dataOpts.source);
+    var sourceSelector = _element.getAttribute(dataOpts.source);
     var sourceContent = responseElements.getElement(sourceSelector);
     sourceContent = (sourceContent) ? sourceContent[0].get('html') : '';
     
-    var destinationSelector = _anchor.getAttribute(dataOpts.destination);
+    var destinationSelector = _element.getAttribute(dataOpts.destination);
     if (!destinationSelector) {
       destinationSelector = sourceSelector;
     }
@@ -86,6 +128,6 @@ PiStory.implement({
     var _destination = document.body.getElement(destinationSelector);
     _destination.set('html', sourceContent);
     
-    eval(_anchor.getAttribute('data-callback'));
+    this.fireEvent('replace', _element);
   }
 });
